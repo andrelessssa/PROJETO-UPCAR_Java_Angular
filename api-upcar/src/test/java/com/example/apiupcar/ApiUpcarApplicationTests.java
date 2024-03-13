313c6852a451ce8d964baa5f245e98cf35564cc4
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-03-13 19:24:38 -0300

    :tada:	init.   iniciada as pastar api e frontend
    :tada:	init.   iniciada as pastar api e frontend

---
+++ api-upcar/src/test/java/com/example/apiupcar/ApiUpcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiupcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUpcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
